ad5c5769d661c1bc34210de469a656484519062f
Reemaalt <reema.altj@gmail.com>
2023-11-27 13:54:44 +0300

    ةة

--- SQL_GUI.java
+++ SQL_GUI.java
@@ -6,6 +6,7 @@ import java.sql.*;
 
 
 public class SQL_GUI extends JFrame {
+    
     private JLabel positionLabel, idLabel;
     private JComboBox<String> positionComboBox;
     private JTextField idTextField;
